dd7279fc6ccd50998aaf188ba42c13eec91ecbae
TaeGyun12 <tg1218.kim@gmail.com>
2022-06-05 11:03:48 +0900

    [Add] 빈 스코프 빈스코프 실습

---
+++ src/main/java/excercise/core/web/LogDemoService.java
@@ -0,0 +1,15 @@
+package excercise.core.web;
+
+import excercise.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
